c2fbed5bfe98fdf8fc64bb0b86fcab67c6f2bde8
KsGr3g0ry <gbrothers29@gmail.com>
2021-02-02 15:26:15 -0600

    command line arguements

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,7 +1,17 @@
 package com.galvanize;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        int x = 45;
+        while (x <= 50){
+            System.out.println("value of i: " + x);
+            x++;
+        }
+        for (int i = 2; i < 7; i+=2) {
+            System.out.println(i);
+        }
     }
 }
+
